74dab30e66454ecd8f38480a5329d0667ce1a17c
Citeli-py <matheus.citeli2001@gmail.com>
2021-11-30 22:26:15 -0300

    Desenha tabuleiro e Inicia tabuleiro

--- Reversi.c
+++ Reversi.c
@@ -6,7 +6,6 @@ typedef struct
     int linha, coluna;
 } coordenada;
 
-
 void ExecutaJogada(int tabuleiro[8][8], int jogador, coordenada jogada)
 {
     /*Se a jogada for válida, a função deve modificar o
@@ -21,6 +20,18 @@ void ExecutaJogada(int tabuleiro[8][8], int jogador, coordenada jogada)
     return 0;
 }
 
+void printa(int *ptr)
+{
+    //notas *(ptr + linha*n_linhas + coluna) == matriz[linha][coluna];
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+            printf("%d ", *(ptr + i*8 + j));
+
+        printf("\n");
+    }
+}
+
 coordenada EscolheJogada()
 {
     coordenada jogada;
@@ -29,9 +40,48 @@ coordenada EscolheJogada()
 }
 
 
-void DesenhaTabuleiro()
+void DesenhaTabuleiro(int tabuleiro[8][8])
 {
+    printf("  ");
+    for (int i = 0; i < 8; i++) // Cria os numeros acima
+    {
+        printf(" %d ",i+1);
+    }
+    
+    printf("\n +------------------------+");
+    for(int i=0; i<8; i++)
+    { 
+        printf("\n%d|",i+1);
+        for(int j=0;j<8;j++) // Define as peças do tabuleiro
+        {
+            if(tabuleiro[i][j] == -1)
+                printf(" B ");
+            if(tabuleiro[i][j] == 1)
+                printf(" W ");
+            if(tabuleiro[i][j] == 0)
+                printf(" - ");
+        }
+        printf("|");
+    }
+    printf("\n +------------------------+");
     return 0;
+}
+
+void IniciaTabuleiro(int *ptr)
+{
+    //Zera o tabuleiro
+    for (int i = 0; i < 8; i++)
+        for (int j = 0; j < 8; j++)
+            *(ptr + i*8 + j) = 0;
+
+
+    //Peças do centro
+    *(ptr + 3*8 + 3) = 1;
+    *(ptr + 4*8 + 3) = -1;
+    *(ptr + 3*8 + 4) = -1;
+    *(ptr + 4*8 + 4) = 1;
+
+    //notas *(ptr + linha*n_linhas + coluna) == matriz[linha][coluna];
 }
 
 int main()
@@ -39,8 +89,11 @@ int main()
 
     coordenada jogada;
     int tabuleiro[8][8];
-    jogada = EscolheJogada();
-    printf("Linha: %d\nColuna: %d", jogada.linha, jogada.coluna);
-
+    //tabuleiro
+    //matriz_teste(&tabuleiro[0][0]);
+    IniciaTabuleiro(&tabuleiro);
+    DesenhaTabuleiro(tabuleiro);
+    //jogada = EscolheJogada();
+    //printf("Linha: %d\nColuna: %d", jogada.linha, jogada.coluna);
     return 0;
 }
