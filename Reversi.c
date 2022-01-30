b87e1747da637d585788539a4068234b41c63fa5
Citeli-py <matheus.citeli2001@gmail.com>
2022-01-30 12:54:29 -0300

    Sentinela

--- Reversi.c
+++ Reversi.c
@@ -3,13 +3,12 @@
 #include <stdio.h>
 
 #define INFINITY 10000
-#define MAX_NIVEL 0
+#define MAX_NIVEL 3
 
 double AvaliaPosicao(struct posicao jogo)
 {
     //quanto melhor estiver o jogo para as peças pretas, menor deve ser o valor retornado;
     // A pontuação vai de -20 até 20, 10 pontos relacionados a quantidade;
-
     double pontos = 0;
     // branco -> +1; preto -> -1
     double brancas = 0,  pretas = 0;
@@ -19,55 +18,39 @@ double AvaliaPosicao(struct posicao jogo)
             if(jogo.tabuleiro[i][j] != 0)
             {
                 if(jogo.tabuleiro[i][j] == 1)
-                    {
                     brancas++;
-                   // printf("Brancas: %.0lf\n", brancas);
-                    }
                 else
-                   {
-                        pretas++;
-              //      printf("pretas: %.0lf\n", pretas);
-                   }
+                    pretas++;
             }
         
-    
     pontos = ((brancas/(brancas+pretas)) * 40.0)-20;
 
-    
     printf("\n|----------(%.2lf)----------|\n", pontos);
     return pontos;
-
 }
 
-
-int printa_lista2(struct elemento *lista)
+struct elemento *Altera_lista(struct elemento *l) // Transforma a lista sem sentinela numa com.
 {
-    struct elemento *p = lista;
-    int i=0;
-    //printf("\n");
+    struct elemento *lista = Cria_sentinela();
+    struct elemento *aux = l;
     do
     {
-        printf("%d-(%d, %d)\n",i+1, p->jog.linha, p->jog.coluna);
-        p = p->prox;
-        i++;
-    }while (p->jog.linha != -1);
-    printf("\n");
-    return i;
-
+        lista = insere(lista, *aux);
+        aux = aux->prox;
+    }while (aux != l);
+    return lista;
 }
-
-
 
 struct jogada ExecutaIA(struct posicao posAtual, int nivel, double alfa, double beta)
 {
     ///declarar e inicializar as variáveis indicadas (item 1 do exercício);
     double melhorValor, valorJogada;
     int podado = 0;
-    struct jogada melhorJogada; 
-    struct elemento jogadaIA;
-    melhorJogada.linha = -1; melhorJogada.coluna = -1;
-    struct elemento *lista = inicializa();
+    struct jogada melhorJogada;
+    struct elemento jogadaIA; 
+    struct elemento *lista;
     struct elemento *jogadaAux;
+    melhorJogada.linha = -1; melhorJogada.coluna = -1;
 
     if(nivel % 2 == 0 )
         melhorValor = -INFINITY;
@@ -76,21 +59,14 @@ struct jogada ExecutaIA(struct posicao posAtual, int nivel, double alfa, double 
         melhorValor = INFINITY;
 
     ///calcular as possíveis jogadas de acordo com o jogador da vez (item 2 e 2a do exercício);
-    struct elemento sentinela;
-    sentinela.jog.linha = -1;
-    lista = insere(lista, sentinela);
-    lista = CalculaJogadasValidas(posAtual);
-    
-    //printa_lista2(lista);
-
+    lista = Altera_lista(CalculaJogadasValidas(posAtual));
+    printa_lista(lista);
     ///a lista sendo vazia, deve ser retornado imediatamente "melhorJogada" (item 2b do exercício);
-    if(lista == NULL)
+    if(CalculaJogadasValidas(posAtual) == NULL)
         return melhorJogada;
 
     jogadaAux = lista->prox;///começar a percorrer a lista, considerando a lista com sentinela (item 3 do exercício)
     struct posicao posCopia;
-
-
 
     while (jogadaAux!=lista && podado == 0)
     {
@@ -107,9 +83,7 @@ struct jogada ExecutaIA(struct posicao posAtual, int nivel, double alfa, double 
             {
                 ///chamar a função recursivamente e guardar a jogada retornada em "jogadaIA" (item 4a do exercício)
                 jogadaIA.jog = ExecutaIA(posCopia, nivel+1, melhorValor, beta);
-
             }
-
             ///verificar se "nivel" é ímpar (item 4b do exercício) - pode ser usado "else" em relação ao item 4a
             else
             {
@@ -120,7 +94,6 @@ struct jogada ExecutaIA(struct posicao posAtual, int nivel, double alfa, double 
             ///executar "jogadaIA" sobre "posCopia" (item 4c do exercício)
             if(jogadaIA.jog.linha>-1)
                 ExecutaJogada(&posCopia, jogadaIA);
-
         }
 
         ///avaliar a posiçao "posCopia" (item 5 do exercício)
@@ -133,7 +106,7 @@ struct jogada ExecutaIA(struct posicao posAtual, int nivel, double alfa, double 
         if(nivel % 2 == 0 && valorJogada>= melhorValor)
         { 
             melhorValor = valorJogada;
-            melhorJogada = jogadaIA.jog;
+            melhorJogada = jogadaAux->jog;
         }
 
         if(nivel % 2 != 0 && valorJogada <= melhorValor)
@@ -146,9 +119,7 @@ struct jogada ExecutaIA(struct posicao posAtual, int nivel, double alfa, double 
     }
 
     ///liberar a memória alocada nas listas de possíveis jogadas das peças brancas ou pretas (item 7 do exercício)
-    destruirlista(lista);
-    destruirlista(jogadaAux);
-
+    destruirlista(lista->prox);
     ///retornar a melhor jogada encontrada "melhorJogada" (item 7 do exercício).
     return melhorJogada;
 
@@ -189,7 +160,6 @@ int main()
 
     while (casasVazias>0)
     {
-       // ExecutaIA(joga, 0, 0, 0);
         DesenhaTabuleiro(joga);
         lista = inicializa();
         lista = CalculaJogadasValidas(joga);
@@ -222,6 +192,7 @@ int main()
 
 
     system("cls");
+    system("pause");
     DesenhaTabuleiro(joga);
     CalculaVencedor(joga.tabuleiro);
     destruirlista(lista);
